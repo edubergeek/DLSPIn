b0911ff8ecc6e8800b93e4c5be541017a5385e19
edubergeek <dodds@hawaii.edu>
2019-03-09 10:15:20 -1000

    Add filtering on solar position and image standard deviation Improved input image normalization (may need to redo as a population statistic instead of per image) Added storing the image name in the TFRecord

--- hinode2tfr.py
+++ hinode2tfr.py
@@ -26,7 +26,7 @@ WStep=5
 
 pTest = 0.1
 pVal = 0.1
-nClone=100
+nCopies=1
 
 train_filename = '../data/train.tfr'  # the TFRecord file containing the training set
 val_filename = '../data/val.tfr'      # the TFRecord file containing the validation set
@@ -35,8 +35,19 @@ test_filename = '../data/test.tfr'    # the TFRecord file containing the test se
 def chunkstring(string, length):
   return (string[0+i:length+i] for i in range(0, len(string), length))
 
-def normalize(img, threshold):
-  val = np.percentile(img,threshold)
+# filter images on mean level 1 intensity pixel value and position on sun
+# True means don't use the image and False means OK to use the image
+def isFiltered(img, nz, meta):
+  if np.std(img[nz]) >= 0.07:
+    yc = float(meta['YCEN'])
+    if yc >= -300.0 and yc <= 300.0:
+      xc = float(meta['XCEN'])
+      if xc >= -700.0 and xc <= 700.0:
+        return False
+  return True
+
+def normalize(img, nz, threshold):
+  val = np.percentile(img[nz],threshold)
   img = img / val
   return img
 
@@ -104,7 +115,7 @@ def process_sp3d(basePath):
         # New image so wrap up the current image
         # Flip image Y axis
         img = np.flip(img, axis=1)
-        yield img, fitsName, level, wl
+        yield img, fitsName, level, wl, meta
       # Initialize for a new image
       #print('Parsing %s - %s'%(imageName, path))
       prevImageName = imageName
@@ -128,39 +139,50 @@ def process_sp3d(basePath):
       # level 2 FITS file
       level = 2
       dimY, dimX, dimZ = imageData.shape
+      # crop to maximum height
+      dimY = min(dimY, YDim)
+      # crop to maximum width
+      dimX = min(dimX, XDim)
       dimW = 0
       #dimZ = 0
       # we should have 3 dimensions, the azimuth, altitude and intensity
       wl = (float(imageMeta['LMIN2']) + float(imageMeta['LMAX2'])) / 2.0
-      img[0,0:dimY,0:dimX,0:dimZ] = imageData
+      img[0,0:dimY,0:dimX,0:dimZ] = imageData[0:dimY,0:dimX,0:dimZ]
+      meta = imageMeta
     else:
       # level 1 FITS file
       level = 1
       x = int(imageMeta['SLITINDX'])
-      wl = float(imageMeta['CRVAL1']) + (WStart*float(imageMeta['CDELT1']))
-      dimZ, dimY, dimX = imageData.shape
-      dimW = WDim
-      # concatenate the next column of data
-      # 4, 512, 112
-      # 1, 512, 9
-      a=np.reshape(imageData[0,:,:],(dimY, dimX))
-      a = a[:,WInd]
-      img[0:dimW,0:dimY,x,0] = np.transpose(a)
-      a=np.reshape(imageData[1,:,:],(dimY, dimX))
-      a = a[:,WInd]
-      img[0:dimW,0:dimY,x,1] = np.transpose(a)
-      a=np.reshape(imageData[2,:,:],(dimY, dimX))
-      a = a[:,WInd]
-      img[0:dimW,0:dimY,x,2] = np.transpose(a)
-      a=np.reshape(imageData[3,:,:],(dimY, dimX))
-      a = a[:,WInd]
-      img[0:dimW,0:dimY,x,3] = np.transpose(a)
+      if x < XDim:
+        wl = float(imageMeta['CRVAL1']) + (WStart*float(imageMeta['CDELT1']))
+        dimZ, dimY, dimX = imageData.shape
+        # crop to maximum height
+        dimY = min(dimY, YDim)
+        # crop to maximum width
+        dimX = min(dimX, XDim)
+        dimW = WDim
+        # concatenate the next column of data
+        # 4, 512, 112
+        # 1, 512, 9
+        a=np.reshape(imageData[0,:dimY,:],(dimY, dimX))
+        a = a[:,WInd]
+        img[0:dimW,0:dimY,x,0] = np.transpose(a)
+        a=np.reshape(imageData[1,:dimY,:],(dimY, dimX))
+        a = a[:,WInd]
+        img[0:dimW,0:dimY,x,1] = np.transpose(a)
+        a=np.reshape(imageData[2,:dimY,:],(dimY, dimX))
+        a = a[:,WInd]
+        img[0:dimW,0:dimY,x,2] = np.transpose(a)
+        a=np.reshape(imageData[3,:dimY,:],(dimY, dimX))
+        a = a[:,WInd]
+        img[0:dimW,0:dimY,x,3] = np.transpose(a)
+        meta = imageMeta
 
   if prevImageName != '':
     # New image so wrap up the current image
     # Flip image Y axis
     img = np.flip(img, axis=1)
-    yield img, fitsName, level, wl
+    yield img, fitsName, level, wl, meta
     fsDetection.close()
   
 def _floatvector_feature(value):
@@ -190,47 +212,64 @@ test_writer = tf.python_io.TFRecordWriter(test_filename)
 # it is critical that pairs are produced reliably first level2 then level1
 # for each level2 (Y) file
 i = nExamples = nTrain = nVal = nTest = 0
-for image, name, level, line in process_sp3d(basePath):
+
+img=np.empty((WDim,YDim,XDim,ZDim))
+#nz=np.empty((ZDim))
+
+for image, name, level, line, meta in process_sp3d(basePath):
   if level == 2:
     # image is the level2 magnetic field prediction per pixel (Y)
     ya=np.reshape(image[0,0:YDim,0:XDim,0:3],(YDim*XDim*3))
-    print(name, level, line)
     nExamples += 1
+    print(nExamples, name, level, line)
   else:
     # image is the level1 Stokes params for several WLs of the line of interest (X)
-    x = normalize(image, 95)
-    #x = image
-    print(name, level, line)
-    #feature = {'magfld': _bytes_feature(tf.compat.as_bytes(y.tostring())), 'stokes': _bytes_feature(tf.compat.as_bytes(x.tostring()))}
-    #xa = np.asarray(x).astype(np.float32)
-    #ya = np.asarray(y).astype(np.float32)
-    xa=np.reshape(image,(WDim*YDim*XDim*ZDim))
-    feature = {'magfld': _floatvector_feature(ya.tolist()), 'stokes': _floatvector_feature(xa.tolist())}
-    # Create an example protocol buffer
-    example = tf.train.Example(features=tf.train.Features(feature=feature))
+    # filter image using first wavelength (axis 0) and SP=I (axis 3)
+    nz0 = image[:,:,:,0].nonzero()
+    nz1 = image[:,:,:,1].nonzero()
+    nz2 = image[:,:,:,2].nonzero()
+    nz3 = image[:,:,:,3].nonzero()
+    img[:,:,:,0] = normalize(image[:,:,:,0], nz0, 95.0)
+    img[:,:,:,1] = normalize(image[:,:,:,1], nz1, 95.0)
+    img[:,:,:,2] = normalize(image[:,:,:,2], nz2, 95.0)
+    img[:,:,:,3] = normalize(image[:,:,:,3], nz3, 95.0)
+    img_filt = image[0,:,:,0]
+    nz_filt = img_filt.nonzero()
+    if isFiltered(img_filt, nz_filt, meta):
+      print(name, level, line, 'Filtered')
+      nExamples -= 1
+    else:
+      print(name, level, line)
+      xa=np.reshape(img,(WDim*YDim*XDim*ZDim))
+      feature = {'magfld': _floatvector_feature(ya.tolist()), 'stokes': _floatvector_feature(xa.tolist()), 'name': _bytes_feature(name.encode('utf-8')) }
+      # Create an example protocol buffer
+      example = tf.train.Example(features=tf.train.Features(feature=feature))
+  
+      # roll the dice to see if this is a train, val or test example
+      # and write it to the appropriate TFRecordWriter
+      for clone in range(nCopies):
+        i += 1
+        roll = np.random.random()
+        if roll >= (pVal + pTest):
+          # Serialize to string and write on the file
+          train_writer.write(example.SerializeToString())
+          nTrain += 1
+        elif roll >= pTest:
+          # Serialize to string and write on the file
+          val_writer.write(example.SerializeToString())
+          nVal += 1
+        else:
+          # Serialize to string and write on the file
+          test_writer.write(example.SerializeToString())
+          nTest += 1
 
-    # roll the dice to see if this is a train, val or test example
-    # and write it to the appropriate TFRecordWriter
-    for clone in range(nClone):
-      roll = np.random.random()
-      if roll >= (pVal + pTest):
-        # Serialize to string and write on the file
-        train_writer.write(example.SerializeToString())
-        nTrain += 1
-      elif roll >= pTest:
-        # Serialize to string and write on the file
-        val_writer.write(example.SerializeToString())
-        nVal += 1
-      else:
-        # Serialize to string and write on the file
-        test_writer.write(example.SerializeToString())
-        nTest += 1
-
-  i += 1
-  if not nExamples % 1000:
-    print('%d examples: %03.1f%% train, %03.1f%%validate, %03.1f%%test.'%(nExamples, 100.0*nTrain/nExamples, 100.0*nVal/nExamples, 100.0*nTest/nExamples))
+  if not nExamples % 100 and i > 0:
+    print('%d examples: %03.1f%% train, %03.1f%%validate, %03.1f%%test.'%(nExamples, 100.0*nTrain/i, 100.0*nVal/i, 100.0*nTest/i))
     sys.stdout.flush()
   
+  #if nExamples == 1000:
+  #  break
+
 train_writer.close()
 val_writer.close()
 test_writer.close()
