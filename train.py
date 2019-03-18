2bcd242653c36e16d828f9911a3e5eaad5487380
Curt Dodds <cdodds@gpu01.ifa.hawaii.edu>
2019-03-18 04:52:04 -1000

    Added another layer (2, one down and one up) Changed filter sizes

--- train.py
+++ train.py
@@ -25,15 +25,16 @@ YMagfld=512
 ZMagfld=3
 
 sizeBatch=2
-nEpochs=15
+nEpochs=20
 nExamples=700
 nValid=72
 
-pathTrain = '../data/train.tfr'  # The TFRecord file containing the training set
-pathValid = '../data/val.tfr'    # The TFRecord file containing the validation set
-pathTest = '../data/test.tfr'    # The TFRecord file containing the test set
-pathWeight = '../data/v1-1.h5'  # The HDF5 weight file generated for the trained model
-pathModel = '../data/v1-1.nn'  # The model saved as a JSON file
+pathTrain = './data/train.tfr'  # The TFRecord file containing the training set
+pathValid = './data/val.tfr'    # The TFRecord file containing the validation set
+pathTest = './data/test.tfr'    # The TFRecord file containing the test set
+pathWeight = './data/v1-1.h5'  # The HDF5 weight file generated for the trained model
+pathModel = './data/v1-1.nn'  # The model saved as a JSON file
+pathLog = '../logs/v1-1'  # The training log
 
 def UNet():
   inputs = Input((WStokes, YDim, XDim, ZStokes))
@@ -43,52 +44,60 @@ def UNet():
   #conv1 = Conv3D(64, (WDim, 1, 1), use_bias=False, padding='same')(conv1)
   #conv1 = BatchNormalization()(conv1)
   #conv1 = Activation("relu")(conv1)
-  conv1 = Conv3D(32, (WDim, 1, 1), activation='relu', padding='same')(inputs)
-  conv1 = Conv3D(32, (WDim, 1, 1), activation='relu', padding='same')(conv1)
-  conv1 = Conv3D(32, (WDim, 7, 7), activation='relu', padding='same')(conv1)
-  conv1 = Conv3D(32, (WDim, 7, 7), activation='relu', padding='same')(conv1)
+  #conv1 = Conv3D(32, (WDim, 1, 1), activation='relu', padding='same')(inputs)
+  conv1 = Conv3D(64, (WDim, 11, 11), activation='relu', padding='same')(inputs)
+  conv1 = Conv3D(64, (WDim, 11, 11), activation='relu', padding='same')(conv1)
+  conv1 = Conv3D(64, (WDim, 11, 11), activation='relu', padding='same')(conv1)
   conv1 = MaxPooling3D(pool_size=(WDim, 1, 1))(conv1)
-  conv1 = Reshape((YDim, XDim, 32))(conv1)
-  conv1 = Conv2D(32, (7, 7), activation='relu', padding='same')(conv1)
-  conv1 = Conv2D(32, (7, 7), activation='relu', padding='same')(conv1)
+  conv1 = Reshape((YDim, XDim, 64))(conv1)
+  conv1 = Conv2D(64, (11, 11), activation='relu', padding='same')(conv1)
+  conv1 = Conv2D(64, (11, 11), activation='relu', padding='same')(conv1)
   pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-  conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-  conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+  conv2 = Conv2D(64, (9, 9), activation='relu', padding='same')(pool1)
+  conv2 = Conv2D(64, (9, 9), activation='relu', padding='same')(conv2)
   pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-  conv3 = Conv2D(100, (3, 3), activation='relu', padding='same')(pool2)
-  conv3 = Conv2D(100, (3, 3), activation='relu', padding='same')(conv3)
+  conv3 = Conv2D(64, (7, 7), activation='relu', padding='same')(pool2)
+  conv3 = Conv2D(64, (7, 7), activation='relu', padding='same')(conv3)
   pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-  conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool3)
-  conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv4)
+  conv4 = Conv2D(128, (5, 5), activation='relu', padding='same')(pool3)
+  conv4 = Conv2D(128, (5, 5), activation='relu', padding='same')(conv4)
   pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
   conv5 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool4)
   conv5 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv5)
+  pool5 = MaxPooling2D(pool_size=(2, 2))(conv5)
 
-  up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
-  conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+  conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool5)
   conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
 
-  up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
-  conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
-  conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+  up7 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv6), conv5], axis=3)
+  conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(up7)
+  conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv7)
 
-  up8 = concatenate([Conv2DTranspose(100, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
-  conv8 = Conv2D(100, (3, 3), activation='relu', padding='same')(up8)
-  conv8 = Conv2D(100, (3, 3), activation='relu', padding='same')(conv8)
+  up8 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv7), conv4], axis=3)
+  conv8 = Conv2D(256, (3, 3), activation='relu', padding='same')(up8)
+  conv8 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv8)
 
-  up9 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
-  conv9 = Conv2D(64, (7, 7), activation='relu', padding='same')(up9)
-  conv9 = Conv2D(64, (7, 7), activation='relu', padding='same')(conv9)
+  up9 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv8), conv3], axis=3)
+  conv9 = Conv2D(128, (5, 5), activation='relu', padding='same')(up9)
+  conv9 = Conv2D(128, (5, 5), activation='relu', padding='same')(conv9)
 
-  conv10 = Conv2D(ZMagfld, (1, 1), activation='linear')(conv9)
+  up10 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv9), conv2], axis=3)
+  conv10 = Conv2D(64, (7, 7), activation='relu', padding='same')(up10)
+  conv10 = Conv2D(64, (7, 7), activation='relu', padding='same')(conv10)
 
-  model = Model(inputs=[inputs], outputs=[conv10])
+  up11 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv10), conv1], axis=3)
+  conv11 = Conv2D(64, (9, 9), activation='relu', padding='same')(up11)
+  conv11 = Conv2D(64, (9, 9), activation='relu', padding='same')(conv11)
 
-  model.compile(optimizer=Adam(lr=1e-4), loss='mse', metrics=['mse'])
+  conv12 = Conv2D(ZMagfld, (1, 1), activation='linear')(conv11)
+
+  model = Model(inputs=[inputs], outputs=[conv12])
+
+  model.compile(optimizer=Adam(lr=3e-5), loss='mse', metrics=['mse'])
 
   print(model.summary())
 
@@ -143,8 +152,8 @@ def train():
   print('-'*30)
   model = UNet()
   callbacks = [
-      tf.keras.callbacks.ModelCheckpoint(pathWeight, verbose=1, save_best_only=True),
-      tf.keras.callbacks.TensorBoard(log_dir='../logs')
+      tf.keras.callbacks.ModelCheckpoint(pathWeight, verbose=1, monitor='val_loss', save_best_only=True),
+      tf.keras.callbacks.TensorBoard(log_dir=pathLog)
   ]
 
   print('-'*30)
